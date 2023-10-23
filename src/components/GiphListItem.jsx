63d35f38c181435d4e7c8186dee5943a59283f62
ivanjorge <ivandcarvalho29@gmail.com>
2023-10-23 11:08:24 +0100

    giph list item done

--- src/components/GiphListItem.jsx
+++ src/components/GiphListItem.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import Card from 'react-bootstrap'
 
-export default function GiphListItem() {
+export default function GiphListItem({ gif }) {
   return (
-    <div>GiphListItem</div>
+    <div className='gifCard'>
+      <img src={gif.images.fixed_height.url} alt={gif.title} />
+      <p className='gifTitle'>{gif.title}</p>
+      </div>
   )
 }
